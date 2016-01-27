116753f67d156083294c0ea4b86aa84eb7a6603d
Edvaldo Szymonek <edvaldoszy@gmail.com>
2016-01-27 17:55:38 -0200

    Ajustado indentação para tabs

--- src/Sys/Sessao.php
+++ src/Sys/Sessao.php
@@ -4,61 +4,61 @@ namespace Sys;
 
 class Sessao
 {
-    public function __construct() {
-
-        if (!isset($_SESSION))
-            session_start();
-    }
-
-    /**
-     * @param string $nome
-     * @return boolean
-     */
-    public function existe($nome) {
-
-        return isset($_SESSION[$nome]);
-    }
-
-    /**
-     * @param string $nome
-     * @param mixed $valor
-     * @return void
-     */
-    public function gravar($nome, $valor) {
-
-        $_SESSION[$nome] = $valor;
-    }
-
-    /**
-     * @param string $nome
-     * @return mixed
-     */
-    public function ler($nome) {
-
-        if ($this->existe($nome))
-            return $_SESSION[$nome];
-
-        return null;
-    }
-
-    /**
-     * @param string $nome
-     * @return void
-     */
-    public function excluir($nome) {
-
-        if ($this->existe($nome))
-            unset($_SESSION[$nome]);
-    }
-
-    /**
-     * @return boolean
-     */
-    public function destruir() {
-
-        if (isset($_SESSION))
-            return session_destroy();
-
-        return false;
-    }
+	public function __construct() {
+		
+		if (!isset($_SESSION))
+			session_start();
+	}
+	
+	/**
+	 * @param string $nome
+	 * @return boolean
+	 */
+	public function existe($nome) {
+		
+		return isset($_SESSION[$nome]);
+	}
+	
+	/**
+	 * @param string $nome
+	 * @param mixed $valor
+	 * @return void
+	 */
+	public function gravar($nome, $valor) {
+		
+		$_SESSION[$nome] = $valor;
+	}
+	
+	/**
+	 * @param string $nome
+	 * @return mixed
+	 */
+	public function ler($nome) {
+		
+		if ($this->existe($nome))
+			return $_SESSION[$nome];
+		
+		return null;
+	}
+	
+	/**
+	 * @param string $nome
+	 * @return void
+	 */
+	public function excluir($nome) {
+		
+		if ($this->existe($nome))
+			unset($_SESSION[$nome]);
+	}
+	
+	/**
+	 * @return boolean
+	 */
+	public function destruir() {
+		
+		if (isset($_SESSION))
+			return session_destroy();
+		
+		return false;
+	}
 }
